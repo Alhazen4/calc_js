e8756822f0c879540ab3c73ec3a9d2fce1afb45d
Alhazen4 <cybernonk404@gmail.com>
2022-05-21 15:35:43 +0700

    Prototype 2

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ function insert(num) {
     document.getElementById('textview').value += num;   
 }
 
-function clear() {
+function clr() {
     document.getElementById('textview').value = '';   
-    document.getElementById('textview').value = 'clear';   
+}
+
+function count() {
+    var calc = document.getElementById('textview').value;
+    document.getElementById('textview').value = eval(calc);
+}
+
+function backspace() {
+    var val = document.getElementById('textview').value;
+    document.getElementById('textview').value = val.substring(0, val.length - 1);
 }
